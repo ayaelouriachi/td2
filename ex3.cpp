3719344f9786596a8b421595365cea7411456138
ayaelouriachi <148795694+ayaelouriachi@users.noreply.github.com>
2023-10-25 00:09:55 +0200

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -1,40 +1,16 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="ex3.cpp" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/ex3.cpp" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/ex3.cpp" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="main.cpp" />
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+int T[10],min=T[0],max=T[0];
+for(int i=0;i<10;i++){
+    cout<<"entrer un nombre";
+    cin>>T[i];
+    if(max<T[i]){
+    max=T[i];}
+     if(min>T[i]){
+    min=T[i];}
+};
+ cout<<"le maximum est"<<max<<"et le minimum est"<<min;
+return 0:}
