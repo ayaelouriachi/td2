20f5d3dbed6bd33e5b9a68befafbd9d86732566c
ayaelouriachi <148795694+ayaelouriachi@users.noreply.github.com>
2023-10-25 00:12:08 +0200

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -1,40 +1,37 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="ex4.cpp" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/ex4.cpp" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/ex4.cpp" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="main.cpp" />
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+
+
+    cout << "Entrez la taille du tableau : ";
+    cin >> n;
+
+
+    int*tableau= new int[n];
+
+    cout << "Entrez " << n << " nombres entiers : ";
+    for (int i = 0; i < n; i++) {
+        cin >> tableau[i];
+    }
+
+    int*tableauCarres = new int[n];
+    for (int i = 0; i < n; i++) {
+        tableauCarres[i] = tableau[i] * tableau[i];
+    }
+cout << "Les carres des nombres sont : ";
+    for (int i = 0; i < n; i++) {
+        cout << tableauCarres[i] << " ";
+    }
+    cout<<endl;
+
+
+    delete[]tableau;
+
+
+    delete[]tableauCarres;
+
+    return 0;
+}
+
+
