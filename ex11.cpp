e795904a3b1b46e365632f8d4fad92eb49bb372c
ayaelouriachi <148795694+ayaelouriachi@users.noreply.github.com>
2023-10-25 00:20:46 +0200

    Update ex11.cpp

--- ex11.cpp
+++ ex11.cpp
@@ -1,43 +1,71 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="ex11.cpp" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/ex11" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/ex11" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="../../../Favorites/Links/o.cpp">
-			<Option target="&lt;{~None~}&gt;" />
-		</Unit>
-		<Unit filename="main.cpp" />
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <iostream>
+#include <cmath>
+using namespace std;
+class Vecteur3d {
+private:
+    float x, y, z;
+
+public:
+
+    Vecteur3d(float x = 0.0, float y = 0.0, float z = 0.0) : x(x), y(y), z(z) {}
+
+    void afficher() {
+        cout << "(" << x << ", " << y << ", " << z << ")" <<endl;
+    }
+
+
+    Vecteur3d somme(const Vecteur3d& other) const {
+        return Vecteur3d(x + other.x, y + other.y, z + other.z);
+   }
+
+    float produitScalaire(const Vecteur3d& other) const {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+
+    bool coincide(const Vecteur3d& other) const {
+        return (x == other.x && y == other.y && z == other.z);
+    }
+
+
+    float norme() const {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+
+    static Vecteur3d normax(const Vecteur3d& v1, const Vecteur3d& v2) {
+        return (v1.norme() > v2.norme()) ? v1 : v2;
+    }
+};
+
+int main() {
+    Vecteur3d v1(1.0, 2.0, 3.0);
+    Vecteur3d v2(3.0, 2.0, 1.0);
+
+    cout << "Vecteur 1 : ";
+    v1.afficher();
+    cout << "Vecteur 2 : ";
+    v2.afficher();
+
+    Vecteur3d somme = v1.somme(v2);
+
+cout << "Somme de vecteur 1 et vecteur 2 : ";
+    somme.afficher();
+
+    float produit = v1.produitScalaire(v2);
+cout << "Produit scalaire de vecteur 1 et vecteur 2 : " << produit <<endl;
+
+    bool coincident = v1.coincide(v2);
+    cout << "Vecteur 1 et vecteur 2 ont-ils les m�mes composantes ? " << (coincident ? "Oui" : "Non") << endl;
+
+    float normeV1 = v1.norme();
+    float normeV2 = v2.norme();
+    cout << "Norme de vecteur 1 : " << normeV1 << endl;
+    cout << "Norme de vecteur 2 : " << normeV2 << endl;
+
+    Vecteur3d vecteurMax = Vecteur3d::normax(v1, v2);
+    cout << "Le vecteur avec la plus grande norme : ";
+    vecteurMax.afficher();
+
+    return 0;
+}
